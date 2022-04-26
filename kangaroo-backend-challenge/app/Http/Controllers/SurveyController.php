6d13c66a57b173cc00183102f301f47cf9af2a88
joe rizk <joerizk2@hotmail.com>
2022-04-26 16:51:12 +0300

    created the get survey api

--- kangaroo-backend-challenge/app/Http/Controllers/SurveyController.php
+++ kangaroo-backend-challenge/app/Http/Controllers/SurveyController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class SurveyController extends Controller
 {
     //
+    public function getSurveys(){
+        $result = array();
+        for ($i = 1; $i < 16; $i++){
+            $path = storage_path() . "/surveys/${i}.json"; 
+            $json = json_decode(file_get_contents($path), true);
+            if (!in_array($json['survey'], $result)){
+                array_push($result,$json['survey']);
+            }
+        
+        }
+        return response()->json($result);
+    }
 }
