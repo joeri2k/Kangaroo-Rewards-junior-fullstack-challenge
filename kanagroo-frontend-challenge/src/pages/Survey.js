cd8521cfabae6b47c556b2116d77d39b55e32c1d
joe rizk <joerizk2@hotmail.com>
2022-04-26 19:27:02 +0300

    fetching survey api

--- kanagroo-frontend-challenge/src/pages/Survey.js
+++ kanagroo-frontend-challenge/src/pages/Survey.js
@@ -1,24 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Table from "../components/Table";
+import axios from "axios";
 
 function Survey() {
-  const data = [
-    {
-      name: "Paris",
-      code: "XX1",
-    },
-    {
-      name: "Melun",
-      code: "XX3",
-    },
-    {
-      name: "Chartres",
-      code: "XX2",
-    },
-  ];
+  //   const data1 = [
+  //     {
+  //       name: "Paris",
+  //       code: "XX1",
+  //     },
+  //     {
+  //       name: "Melun",
+  //       code: "XX3",
+  //     },
+  //     {
+  //       name: "Chartres",
+  //       code: "XX2",
+  //     },
+  //   ];
+  const [data, setData] = useState("");
+  async function getSurveys() {
+    const url = "http://127.0.0.1:8001/api/surveys";
+    try {
+      const response = await axios.get(url);
+      const data_received = await response.data;
+      console.log(data_received);
+      setData(data_received);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    getSurveys();
+  }, []);
   return (
     <div className="container">
-      <Table data={data} />
+      {/* <div>{data1}</div> */}
+      <h1>title</h1>
+      <Table data={data && data} />
     </div>
   );
 }
