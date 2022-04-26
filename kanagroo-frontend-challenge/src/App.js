6ffbdd1d4bcd2d7548bc1c9b063004903206648e
joe rizk <joerizk2@hotmail.com>
2022-04-26 17:55:32 +0300

    frontend setup

---
+++ kanagroo-frontend-challenge/src/App.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Surveys from "./pages/Surveys";
+
+function App() {
+  return (
+    <div>
+      <Surveys />
+    </div>
+  );
+}
+
+export default App;
---
+++ kanagroo-frontend-challenge/src/pages/Survey.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Surveys() {
+  return (
+    <div>
+      <div>test</div>
+    </div>
+  );
+}
+
+export default Surveys;
